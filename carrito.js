1da2ed23bef5476cda8a36b640d22125acf903c3
Angel Resendiz <angel.resendiz@gmail.com>
2021-11-26 19:33:48 -0600

    Add cart

--- carrito.js
+++ carrito.js
@@ -1,6 +1,36 @@
+console.clear();
 class Cart {
   //// Tu código empieza aquí ///
-
+  constructor() {
+    this.qty = 0;
+    this.totalPrice = 0;
+  }
+  addToCart(_, price) {
+    this.qty = this.qty + 1;
+    this.totalPrice += price;
+  }
+  removeFromCart(_, price) {
+    this.totalPrice -= price;
+    this.qty = this.qty - 1;
+  }
+  emptyCart() {
+    this.qty = 0;
+    this.totalPrice = 0;
+  }
+  printItems() {
+    if (this.qty === 0) {
+      console.log("Tu carrito está vacío");
+    } else {
+      const wtf =
+        (this.qty === 0 && this.totalPrice > 0) ||
+        (this.totalPrice === 0 && this.qty > 0)
+          ? "WTF?!!"
+          : "";
+      console.log(
+        `Tu carrito tiene ${this.qty} artículos por un total de $${this.totalPrice} MXN ${wtf}`
+      );
+    }
+  }
 
   //// Tu código termina aquí ///
 }
@@ -24,6 +54,7 @@ carrito.printItems();
 // Nice to have:
 carrito.addToCart("Tamal Verde", 30);
 carrito.addToCart("Tamal Verde", 30);
-carrito.removeFromCart("TamalVerde", 60);
+carrito.printItems();
+carrito.removeFromCart("Tamal Verde", 60);
 carrito.printItems();
 // Tu carrito tiene 1 artículos por un total de $0 MXN WTF!??
--- clases.js
+++ clases.js
@@ -7,60 +7,63 @@ class User {
     this.token = "";
   }
   printInfo() {
-    if (this.token !== "")console.log(`->${this.username} (${this.email})`);
+    if (this.token !== "") console.log(`->${this.username} (${this.email})`);
     else console.log("Error: No ha iniciado sesión");
+  }
+  changeEmail(newEmail) {
+    this.email = newEmail;
   }
   login(username, pass) {
     if (username === this.username && pass === "secreto") {
-      this.token = "lsjdflaksjdf489789";
+      if (true) {
+        this.token = "lsjdflaksjdf489789";
+      }
     } else {
       console.log("Error al iniciar sesión");
     }
   }
-  logout(){
-    this.token = ""
+  logout() {
+    this.token = "";
   }
 }
 
 class Admin extends User {
-  restartSystem(){
-    console.log('Reiniciando el sistema')
+  restartSystem() {
+    console.log("Reiniciando el sistema");
   }
 }
 
 class Customer extends User {
-  buy(producto){
-    console.log(`Comprando ${producto}`)
+  buy(producto) {
+    console.log(`Comprando ${producto}`);
   }
 }
 
 class Cashier extends Customer {
-  cancel(producto){
-    console.log(`Cancelando ${producto}`)
+  cancel(producto) {
+    console.log(`Cancelando ${producto}`);
   }
 }
 
 const juan = new User("juanperez", "juan.perez@gmail.com");
 juan.login("juanperez", "secreto");
+juan.changeEmail("juanito@gmail.com");
 juan.printInfo();
 juan.logout();
 juan.printInfo();
 
-const angel = new Admin("angelprg", "angel.resendiz@gmail.com")
+const angel = new Admin("angelprg", "angel.resendiz@gmail.com");
 angel.login("angelprg", "secreto");
 angel.printInfo();
 angel.restartSystem();
 
-const pepe = new Customer("pepepecas", "pepe@gmail.com")
+const pepe = new Customer("pepepecas", "pepe@gmail.com");
 pepe.login("pepepecas", "secreto");
 pepe.printInfo();
-pepe.buy("Tamal")
+pepe.buy("Tamal");
 
-const pepeCajero = new Cashier("pepepecas", "pepe@gmail.com")
+const pepeCajero = new Cashier("pepepecas", "pepe@gmail.com");
 pepeCajero.login("pepepecas", "secreto");
 pepeCajero.printInfo();
-pepeCajero.buy("Tamal")
-pepeCajero.cancel("Tamal")
-
-
-
+pepeCajero.buy("Tamal");
+pepeCajero.cancel("Tamal");
